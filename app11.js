fec4b2d51392c924e0899b8870e7bf4fc87eef2b
Shubham_khalane <118313700+1khalaneshubham@users.noreply.github.com>
2023-12-30 10:23:05 +0530

    Add files via upload

---
+++ app11.js
@@ -0,0 +1,20 @@
+// Arithmetic Operators
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+// Unary
+console.log(a++);
+console.log(++b);
+
+// Assignment 
+// a = b;
+b = a;
+console.log(a) // 11
+console.log(b) // 11
+
---
+++ app12.js
@@ -0,0 +1,6 @@
+
+let pencilePrice = 10;
+let erasorPrice = 5;
+// let outPut = "The total price is: "+(pencilePrice + erasorPrice)+" Rupees.";
+// let outPut =  // back ticks
+console.log(`The total price is: ${pencilePrice + erasorPrice} Rupees.`);
