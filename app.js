81fcf4653bcb7a39fc770a64c25a500adb0222df
Shubham_khalane <118313700+1khalaneshubham@users.noreply.github.com>
2023-12-30 10:34:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-let msg = "artISlife";
-console.log(msg.slice(0,4));
-console.log(msg.slice());
-console.log(msg.slice(-2));
+// condition Statement
+console.log("Before my if Statement :)");
+let age = 23;
+if (age >= 18){
+    console.log("You can Vote");
+    console.log("You can Drive");
+    let a = 5;
+    console.log(5 * a);
+}
+console.log("After my if Statement :)");
